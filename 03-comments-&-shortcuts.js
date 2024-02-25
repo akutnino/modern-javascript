9a59cd93cd3848574b3e30824466f98ca63c5797
Nino Akut <akutnino@gmail.com>
2024-02-26 05:14:38 +0800

    Comments & Shortcuts updated

---
+++ 03-comments-&-shortcuts.js
@@ -0,0 +1,28 @@
+// This is a single line of code.
+
+/*
+Multi-Line Comment
+console.log('Hello World!');
+
+console.log(100);
+
+console.log(10, 'Hello', true);
+*/
+
+const x = 100;
+
+console.log(x);
+
+console.error('This is an Error');
+
+console.warn('This is a Warning');
+
+console.table({ fname: 'foobar', email: 'foobar@email.com' });
+
+console.group('sample');
+
+console.log('Hello World!');
+console.log(100);
+console.log(10, 'Hello', true);
+
+console.groupEnd();
